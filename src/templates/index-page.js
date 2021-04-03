d1ed43113cf854766de43c78038079226577e5d6
bwthomas <bwthomas@gmail.com>
2021-04-03 12:02:47 -0500

    Better articulate 'mismanagement'

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -31,16 +31,40 @@ export const IndexPageTemplate = ({
               </div>
               <div>
                 <p>
-                  Our school district has been mismanaged. Over the years our finances have
-                  deteriorated to the point of crisis, and most recently with the COVID-19
-                  pandemic, but also in terms of the lack of alignment on priorities between
-                  the school board and members of the community most directly impacted, namely
-                  students, parents and educators. As a member of the community, I have for
-                  years been a consistent advocate for better management, but my conscience
-                  has pushed me to seek a more active role.
+                  Our district's track record of focusing on the wants of today over the needs of
+                  tomorrow has allowed finances to reach a point of crisis. Without passage of
+                  the <a href="http://yeswths.com/">referendum</a> the district will struggle to
+                  provide a level of service to the community that honors the effort, investment
+                  and engagement that students and teachers bring to school every day. We see this
+                  brand of thought most recently with the push to return to classrooms when metrics
+                  from both the CDC and the Lake County Health Department told us that we were at
+                  "<em><a href='https://docs.google.com/presentation/d/1mY8layXi_PmHtL89Gb5B3SmA60Ch4HYH2xJY0Ko-Ggc/edit?usp=sharing'>
+                  the highest risk of community transmission in schools</a></em>," but this is a moment
+                  decades in the making.
                 </p>
                 <p>
-                  You can read more about my <a href="/platform">platform</a>, connect with <a href="https://twitter.com/dijjnn">me</a> or my <a href="https://www.facebook.com/ElectBlakeThomas">campaign</a>.
+                  We need District leadership that can do the work to support the community by
+                  planning ahead. And yet, district leadership has historically and consistently
+                  shown a lack of engagement with the community most directly impacted by their
+                  decisions—namely students, parents and educators. It's this lack of engagement
+                  that leaves us ill-prepared to meet the moment and makes it that much more
+                  critically important that we pass the referendum.
+                </p>
+                <p>
+                  It's been said that "<em>poor planning on your part does not necessitate an
+                  emergency on mine.</em>" There's wisdom in this, but it doesn't remove the
+                  urgency of the issues and instead invites us to consider who we are. Do we
+                  take responsibility as a community ready to meet those challenges?
+                </p>
+                <p>
+                  As a member of the community, I have for years been a consistent advocate for
+                  better management, but my conscience has pushed me to seek a more active role.
+                  I believe we can meet the moment together, and I plan to do
+                  it with the community's support. You can read more about my <a href="/platform">
+                  platform</a>, connect with <a href="https://twitter.com/dijjnn">me</a> or my
+                  <a href="https://www.facebook.com/ElectBlakeThomas">campaign</a>.
+                </p>
+                <p>
                 </p>
               </div>
             </div>
